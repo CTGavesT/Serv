f33772c77bed808c6d5401a70b4ba60fdacc5071
CTGavesT <35978951+CTGavesT@users.noreply.github.com>
2021-03-06 23:02:20 +0200

    Add files via upload

---
+++ Codes/CountdownTimer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    public float Timer;
+    public float TimerLimit = 30.0f;
+    public Text TimerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Timer = TimerLimit;
+        TimerText.text = "Timer Left: " + Timer.ToString("f0");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer -= Time.deltaTime; //Timer = Timer - Time.deltaTime
+        SetTimer();
+
+        if (Timer <= 0.0f)
+        {
+            print("you lose!");
+            Timer = TimerLimit;
+        }
+    }
+
+    void SetTimer()
+    {
+        TimerText.text = "Timer Left: " + Timer.ToString("f0");
+    }
+}
---
+++ Codes/MessagePanel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessagePanel : MonoBehaviour
+{
+     public GameObject MessagePanels;
+
+    public void OpenMessagePanel(string text)
+    {
+        MessagePanels.SetActive(true);
+    }
+
+    public void OpenMessagePanel()
+    {
+        MessagePanels.SetActive(false);
+    }
+
+}
